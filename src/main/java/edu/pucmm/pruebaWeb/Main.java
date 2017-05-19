0e820ae4470b312b5508df4cbec74a8be2ec308a
Manuel Tolentino <manutm24@gmail.com>
2017-05-19 11:47:17 -0400

    Enviando proyecto a GitHub

---
+++ src/main/java/edu/pucmm/pruebaWeb/Main.java
@@ -0,0 +1,11 @@
+package edu.pucmm.pruebaWeb;
+
+/**
+ * Created by mt on 19/05/17.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo!!");
+    }
+
+}
